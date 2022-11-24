c3785e659c6a7d7fa98db627357b157ca444ab5d
bluenote-1577 <jimshawster@gmail.com>
2022-11-24 22:08:02 +0000

    use skani static instead

--- time_2328_results.sh
+++ time_2328_results.sh
@@ -2,7 +2,7 @@ threads=20
 bintime_var=/bin/time
 mkdir 2328_sourmash_sketches
 
-$bintime_var -v -o times/skani_2328_triangle.time skani triangle -t$threads 2328/* > results/skani_2328
+$bintime_var -v -o times/skani_2328_triangle.time ./skani triangle -t$threads 2328/* > results/skani_2328
 $bintime_var -v -o times/mash_2328_triangle.time mash triangle -p $threads 2328/*.fa > results/mash_2328
 $bintime_var -v -o times/fastani_2328_triangle.time fastANI --rl ./2328_l.txt --ql ./2328_l.txt -t $threads -o results/fastani_2328 --matrix
 $bintime_var -v -o times/sourmash_2328_sketch.time parallel -j $threads -a ./sourmash_2328_sketch_cmd.txt
