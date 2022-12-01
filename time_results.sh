c23157cd51f3cddca6cba7cf8a61a9bf01218c5d
bluenote-1577 <jimshawster@gmail.com>
2022-12-01 01:19:37 +0000

    use path skani instead

--- time_results.sh
+++ time_results.sh
@@ -5,21 +5,21 @@ mkdir refseq_sourmash_sketches
 rm ./e.coli-K12.fasta.sig
 
 #gtdb
-$bintime_var -v -o times/skani_gtdb_sketch.time ./skani sketch -o gtdb_skani_sketch -l gtdb_all.txt -t$threads
-$bintime_var -v -o times/skani_gtdb_query.time ./skani search -d gtdb_skani_sketch data/e.coli-K12.fasta -o results/gtdb_skani.txt -t$threads
+$bintime_var -v -o times/skani_gtdb_sketch.time skani sketch -o gtdb_skani_sketch -l gtdb_all.txt -t$threads
+$bintime_var -v -o times/skani_gtdb_query.time skani search -d gtdb_skani_sketch data/e.coli-K12.fasta -o results/gtdb_skani.txt -t$threads
 $bintime_var -v -o times/mash_gtdb_sketch.time mash sketch -o gtdb_mash_sketch -l gtdb_all.txt -p $threads 
 $bintime_var -v -o times/mash_gtdb_dist.time mash dist gtdb_mash_sketch.msh data/e.coli-K12.fasta -p $threads > results/gtdb_mash.txt
 
 #d3
-$bintime_var -v -o times/skani_D3_sketch.time ./skani sketch -o D3_skani_sketch ./references/D3/* -t$threads
-$bintime_var -v -o times/skani_D3_query.time ./skani search -d D3_skani_sketch data/e.coli-K12.fasta -o ./results/D3_skani.txt -t$threads
+$bintime_var -v -o times/skani_D3_sketch.time skani sketch -o D3_skani_sketch ./references/D3/* -t$threads
+$bintime_var -v -o times/skani_D3_query.time skani search -d D3_skani_sketch data/e.coli-K12.fasta -o ./results/D3_skani.txt -t$threads
 $bintime_var -v -o times/mash_D3_sketch.time mash sketch -o D3_mash_sketch ./references/D3/*.fna -p $threads 
 $bintime_var -v -o times/mash_D3_dist.time mash dist D3_mash_sketch.msh data/e.coli-K12.fasta -p $threads > results/D3_mash.txt
 $bintime_var -v -o times/fastani_D3.time fastANI --rl ./d3_l.txt -q data/e.coli-K12.fasta -t $threads -o results/D3_fastani.txt  2> times/fastani_D3_log.txt
 
 #seq_aai
-$bintime_var -v -o times/skani_refseq_sketch.time ./skani sketch -o refseq_skani_sketch references/refseq_all/* -t$threads -a
-$bintime_var -v -o times/skani_refseq_query.time ./skani search -d refseq_skani_sketch data/e.coli-K12.fasta -o ./results/refseq_skani_aai-k12.txt -t$threads
+$bintime_var -v -o times/skani_refseq_sketch.time skani sketch -o refseq_skani_sketch references/refseq_all/* -t$threads -a
+$bintime_var -v -o times/skani_refseq_query.time skani search -d refseq_skani_sketch data/e.coli-K12.fasta -o ./results/refseq_skani_aai-k12.txt -t$threads
 rm -r fastaai_refseq_all
 $bintime_var -v -o times/fastaai_refseq_sketch.time fastaai build_db -g references/refseq_all/ -o fastaai_refseq_all --threads 20
 $bintime_var -v -o times/fastaai_refseq_dist.time fastaai simple_query -g data/e.coli-K12.fasta --target fastaai_refseq_all/database/FastAAI_database.sqlite.db -o tt
