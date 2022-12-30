c2f1667129f5ff17bc3f19111f75a9e29fbb445b
jim <jimshawster@gmail.com>
2022-12-29 23:16:41 -0500

    added refseq to results script

--- time_results.sh
+++ time_results.sh
@@ -17,17 +17,25 @@ $bintime_var -v -o times/mash_D3_sketch.time mash sketch -o D3_mash_sketch ./ref
 $bintime_var -v -o times/mash_D3_dist.time mash dist D3_mash_sketch.msh data/e.coli-K12.fasta -p $threads > results/D3_mash.txt
 $bintime_var -v -o times/fastani_D3.time fastANI --rl ./d3_l.txt -q data/e.coli-K12.fasta -t $threads -o results/D3_fastani.txt  2> times/fastani_D3_log.txt
 
+#d3
+$bintime_var -v -o times/skani_refseq_sketch.time ./skani sketch -o refseq_skani_sketch ./references/refseq/* -t$threads
+$bintime_var -v -o times/skani_refseq_query.time ./skani triangle ./refseq_skani_sketch/* -o ./results/refseq_skani-variety.txt -t$threads
+$bintime_var -v -o times/mash_refseq_sketch.time mash sketch -o refseq_mash_sketch ./references/refseq/*.fna -p $threads 
+$bintime_var -v -o times/mash_refseq_dist.time mash triangle refseq_mash_sketch.msh -E -p $threads > results/refseq_mash-variety.txt
+$bintime_var -v -o times/fastani_refseq.time fastANI --rl ./refseq.txt --ql ./refseq.txt -t $threads -o results/refseq_fastani-variety.txt  2> times/fastani_refseq_log.txt
+
+
 #seq_aai
-$bintime_var -v -o times/skani_refseq_sketch.time ./skani sketch -o refseq_skani_sketch references/refseq_all/* -t$threads -a
-$bintime_var -v -o times/skani_refseq_query.time ./skani search -d refseq_skani_sketch data/e.coli-K12.fasta -o ./results/refseq_skani_aai-k12.txt -t$threads
-rm -r fastaai_refseq_all
-$bintime_var -v -o times/fastaai_refseq_sketch.time fastaai build_db -g references/refseq_all/ -o fastaai_refseq_all --threads 20
-$bintime_var -v -o times/fastaai_refseq_dist.time fastaai simple_query -g data/e.coli-K12.fasta --target fastaai_refseq_all/database/FastAAI_database.sqlite.db -o tt
-mv ./tt/results/e_coli_K12_results.txt results/refseq_fastaai-k12.txt
-$bintime_var -v -o times/sourmash_refseq_sketch.time parallel -j $threads -a ./sourmash_sketch_cmd.txt 
-sourmash sketch translate data/e.coli-K12.fasta
-$bintime_var -v -o times/sourmash_refseq_search.time sourmash search ./e.coli-K12.fasta.sig ./refseq_sourmash_sketches/*.sig --max-containment -t 0.0 -n0 -o results/refseq_sourmash_aai-k12.txt
-
-
+#$bintime_var -v -o times/skani_refseq_sketch.time ./skani sketch -o refseq_skani_sketch references/refseq_all/* -t$threads -a
+#$bintime_var -v -o times/skani_refseq_query.time ./skani search -d refseq_skani_sketch data/e.coli-K12.fasta -o ./results/refseq_skani_aai-k12.txt -t$threads
+#rm -r fastaai_refseq_all
+#$bintime_var -v -o times/fastaai_refseq_sketch.time fastaai build_db -g references/refseq_all/ -o fastaai_refseq_all --threads 20
+#$bintime_var -v -o times/fastaai_refseq_dist.time fastaai simple_query -g data/e.coli-K12.fasta --target fastaai_refseq_all/database/FastAAI_database.sqlite.db -o tt
+#mv ./tt/results/e_coli_K12_results.txt results/refseq_fastaai-k12.txt
+#$bintime_var -v -o times/sourmash_refseq_sketch.time parallel -j $threads -a ./sourmash_sketch_cmd.txt 
+#sourmash sketch translate data/e.coli-K12.fasta
+#$bintime_var -v -o times/sourmash_refseq_search.time sourmash search ./e.coli-K12.fasta.sig ./refseq_sourmash_sketches/*.sig --max-containment -t 0.0 -n0 -o results/refseq_sourmash_aai-k12.txt
+#
+#
 
 
